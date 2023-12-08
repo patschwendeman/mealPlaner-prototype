667abf349ecfa6534fb4a081452e246b4e61f486
Patrick Schwendemann <patrick.schwendemann@bamero.de>
2023-12-08 22:25:45 +0100

    exported bottomNavBarButton

--- app/lib/shared/widgets/bottom_nav/bottom_nav.dart
+++ app/lib/shared/widgets/bottom_nav/bottom_nav.dart
@@ -1,8 +1,9 @@
 import 'package:app/features/home/view/home_screen.dart';
 import 'package:app/features/meal_plans/view/mealplans_screen.dart';
-import 'package:app/features/searchScreen.dart';
+import 'package:app/features/search/view/search_screen.dart';
+
 import 'package:app/shared/variables/colors.dart';
-import 'package:app/shared/widgets/bottem_nav-button.dart';
+import 'package:app/shared/widgets/bottem_nav_button.dart';
 import 'package:flutter/material.dart';
 
 class BottomNav extends StatelessWidget implements PreferredSizeWidget {
