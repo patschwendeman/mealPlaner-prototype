d9eb936587ba8e50c3239a43f74a4b4b9bb5d101
Patrick Schwendemann <patrick.schwendemann@bamero.de>
2023-12-10 00:13:01 +0100

    Formated code

--- app/lib/features/home/widgets/daily_bars_card.dart
+++ app/lib/features/home/widgets/daily_bars_card.dart
@@ -6,9 +6,6 @@ import 'package:flutter/material.dart';
 class DailyBarsCard extends StatefulWidget {
   const DailyBarsCard({super.key});
 
-  
-
-
   @override
   State<DailyBarsCard> createState() => _DailyBarsCardState();
 }
@@ -17,31 +14,27 @@ class _DailyBarsCardState extends State<DailyBarsCard> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: MediaQuery.of(context).size.width,
-      padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
-      margin: EdgeInsets.symmetric(vertical: 8.0),
-      decoration: BoxDecoration(
-        color: AppColors.white,
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-      child: Column(
-        children: [
-          MacroBar(
-              color: AppColors.green, width: 1, amound: '100%', label: ''),
+        width: MediaQuery.of(context).size.width,
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+        margin: const EdgeInsets.symmetric(vertical: 8.0),
+        decoration: BoxDecoration(
+          color: AppColors.white,
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: const Column(children: [
+          MacroBar(color: AppColors.green, width: 1, amound: '100%', label: ''),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      DailyBar(color: AppColors.red, label: 'M'),
-                      DailyBar(color: AppColors.green, label: 'T'),
-                      DailyBar(color: AppColors.green, label: 'W'),
-                      DailyBar(color: AppColors.green, label: 'T'),
-                      DailyBar(color: AppColors.red, label: 'F'),
-                      DailyBar(color: AppColors.green, label: 'S'),
-                      DailyBar(color: AppColors.grey, label: 'S'),
-                    ],
-                  ),
-                  ]
-      )
-    );
+            children: [
+              DailyBar(color: AppColors.red, label: 'M'),
+              DailyBar(color: AppColors.green, label: 'T'),
+              DailyBar(color: AppColors.green, label: 'W'),
+              DailyBar(color: AppColors.green, label: 'T'),
+              DailyBar(color: AppColors.red, label: 'F'),
+              DailyBar(color: AppColors.green, label: 'S'),
+              DailyBar(color: AppColors.grey, label: 'S'),
+            ],
+          ),
+        ]));
   }
 }
