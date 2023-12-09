0c067033884eff998d671cfa03f680336faf4ccd
Patrick Schwendemann <patrick.schwendemann@bamero.de>
2023-12-10 00:09:43 +0100

    add Filtercategories for filterBar in search_screen

---
+++ app/lib/shared/variables/filter_categories.dart
@@ -0,0 +1,13 @@
+import 'package:app/shared/variables/colors.dart';
+
+class FilterCategories {
+  static const List<Map<String, dynamic>> filterCategoriesList = [
+    {'label': 'Vegan', 'color': AppColors.green},
+    {'label': 'Veggie', 'color': AppColors.green},
+    {'label': 'High Protein', 'color': AppColors.red},
+    {'label': 'Low Carb', 'color': AppColors.blue},
+    {'label': 'Low Fat', 'color': AppColors.yellow},
+  ];
+}
+
+
