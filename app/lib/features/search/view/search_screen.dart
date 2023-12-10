b8182475ac6fe776679bb3e5d1a8e29479b1c54e
Patrick Schwendemann <patrick.schwendemann@bamero.de>
2023-12-10 14:36:56 +0100

    Added flutter bloc

--- app/lib/features/search/view/search_screen.dart
+++ app/lib/features/search/view/search_screen.dart
@@ -1,11 +1,16 @@
+import 'package:app/features/search/bloc/search_bloc.dart';
 import 'package:app/features/search/view/search_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SearchView();
+    return BlocProvider<SearchBloc>(
+      create: (context) => SearchBloc(),
+      child: const SearchView()
+    );
   }
 }
